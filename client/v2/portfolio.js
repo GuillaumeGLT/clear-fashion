8558cf2ba207b2b4cdbcab436b8bf4f9d153da1b
Guillaume Gallet <guillaume.gallet@edu.devinci.fr>
2023-03-21 12:17:02 +0100

    test

--- client/v2/portfolio.js
+++ client/v2/portfolio.js
@@ -1,5 +1,5 @@
 // Invoking strict mode https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Strict_mode#invoking_strict_mode
-"use strict";
+'use strict';
 
 /*
 Description of the available api
@@ -22,19 +22,24 @@ let currentProducts = [];
 let currentPagination = {};
 
 // instantiate the selectors
-const selectShow = document.querySelector("#show-select");
-const selectPage = document.querySelector("#page-select");
-const sectionProducts = document.querySelector("#products");
-const spanNbProducts = document.querySelector("#nbProducts");
-const selectBrand = document.querySelector("#brand-select");
-const buttonRecent = document.querySelector("#recent");
+const selectShow = document.querySelector('#show-select');
+const selectPage = document.querySelector('#page-select');
+const sectionProducts = document.querySelector('#products');
+const spanNbProducts = document.querySelector('#nbProducts');
+const selectBrand = document.querySelector('#brand-select');
+const spanNbBrands = document.querySelector('#nbBrands');
+const selectReleased = document.querySelector('#released-select');
+const selectPrice = document.querySelector('#price-select');
+const selectSort = document.querySelector('#sort-select');
+const spanNbNewProducts = document.querySelector('#nbNewProducts');
+//var selectFavorite = document.querySelector('#cb${counter}');
 
 /**
  * Set global value
  * @param {Array} result - products to display
  * @param {Object} meta - pagination meta info
  */
-const setCurrentProducts = ({ result, meta }) => {
+const setCurrentProducts = ({result, meta}) => {
   currentProducts = result;
   currentPagination = meta;
 };
@@ -43,7 +48,6 @@ const setCurrentProducts = ({ result, meta }) => {
  * Fetch products from api
  * @param  {Number}  [page=1] - current page to fetch
  * @param  {Number}  [size=12] - size of the page
- * @param  {Boolean} [recent=false] - filter by recent products
  * @return {Object}
  */
 const fetchProducts = async (page = 1, size = 12) => {
@@ -55,13 +59,13 @@ const fetchProducts = async (page = 1, size = 12) => {
 
     if (body.success !== true) {
       console.error(body);
-      return { currentProducts, currentPagination };
+      return {currentProducts, currentPagination};
     }
 
     return body.data;
   } catch (error) {
     console.error(error);
-    return { currentProducts, currentPagination };
+    return {currentProducts, currentPagination};
   }
 };
 
@@ -69,24 +73,27 @@ const fetchProducts = async (page = 1, size = 12) => {
  * Render list of products
  * @param  {Array} products
  */
-const renderProducts = (products) => {
+const renderProducts = products => {
   const fragment = document.createDocumentFragment();
-  const div = document.createElement("div");
+  const div = document.createElement('div');
+  var counter = 0;
   const template = products
-    .map((product) => {
+    .map(product => {
+      counter++;
       return `
       <div class="product" id=${product.uuid}>
         <span>${product.brand}</span>
-        <a href="${product.link}">${product.name}</a>
+        <a href="${product.link}" target="_blank">${product.name}</a>
         <span>${product.price}</span>
+        <input id="cb${counter}" class="star" type="checkbox" title="bookmark page" checked>
       </div>
     `;
     })
-    .join("");
+    .join('');
 
   div.innerHTML = template;
   fragment.appendChild(div);
-  sectionProducts.innerHTML = "<h2>Products</h2>";
+  sectionProducts.innerHTML = '<h2>Products</h2>';
   sectionProducts.appendChild(fragment);
 };
 
@@ -94,12 +101,12 @@ const renderProducts = (products) => {
  * Render page selector
  * @param  {Object} pagination
  */
-const renderPagination = (pagination) => {
-  const { currentPage, pageCount } = pagination;
+const renderPagination = pagination => {
+  const {currentPage, pageCount} = pagination;
   const options = Array.from(
-    { length: pageCount },
+    {'length': pageCount},
     (value, index) => `<option value="${index + 1}">${index + 1}</option>`
-  ).join("");
+  ).join('');
 
   selectPage.innerHTML = options;
   selectPage.selectedIndex = currentPage - 1;
@@ -109,10 +116,40 @@ const renderPagination = (pagination) => {
  * Render page selector
  * @param  {Object} pagination
  */
-const renderIndicators = (pagination) => {
-  const { count } = pagination;
+const renderIndicators = pagination => {
+  const {count} = pagination;
 
   spanNbProducts.innerHTML = count;
+};
+
+const fetchBrands = async () => {
+  try {
+    const response = await fetch(
+      `https://clear-fashion-api.vercel.app/brands`
+    );
+    const body = await response.json();
+
+    if (body.success !== true) {
+      console.error(body);
+      return {currentProducts, currentPagination};
+    }
+
+    return body.data.result;
+  } catch (error) {
+    console.error(error);
+    return {currentProducts, currentPagination};
+  }
+};
+
+const renderBrands = async() => {
+  const brandsname = await fetchBrands();
+  spanNbBrands.innerHTML = brandsname.length;
+  brandsname.unshift('All');
+  const options = Array.from(brandsname,
+    (brand => `<option value="${brand}">${brand}</option>`)
+  ).join('');
+    
+  selectBrand.innerHTML = options;
 };
 
 const render = (products, pagination) => {
@@ -125,242 +162,242 @@ const render = (products, pagination) => {
  * Declaration of all Listeners
  */
 
-/**
+/** Feature 0 - Show more
+As a user
+I want to show more products
+So that I can display 12, 24 or 48 products on the same page
+
  * Select the number of products to display
  */
-selectShow.addEventListener("change", async (event) => {
-  const products = await fetchProducts(
-    currentPagination.currentPage,
-    parseInt(event.target.value)
-  );
+selectShow.addEventListener('change', async (event) => {
+  const products = await fetchProducts(currentPagination.currentPage, parseInt(event.target.value));
 
   setCurrentProducts(products);
   render(currentProducts, currentPagination);
 });
 
-document.addEventListener("DOMContentLoaded", async () => {
-  const products = await fetchProducts();
 
-  setCurrentProducts(products);
-  render(currentProducts, currentPagination);
-});
 
-/**feature 1
+/**Feature 1 - Browse pages
+As a user
+I want to browse available pages
+So that I can load more products 
+ * Select the page to display */
 
 selectPage.addEventListener('change', async (event) => {
-  const page = parseInt(event.target.value);
-  const products = await fetchProducts(page);
+  const products = await fetchProducts(parseInt(event.target.value));
 
   setCurrentProducts(products);
   render(currentProducts, currentPagination);
 });
-*/
 
-/**feature 2
- * Filter products by brand
- * @param  {String} brand - brand name to filter by
- * @param  {Array} products - array of products to filter
- * @return {Array} - filtered array of products
-
-const filterProducts = (brand, products) => {
-  if (!brand) return products;
-  return products.filter(product => product.brand === brand);
-}
-*/
-
-/**feature 3
-const fetchProducts = async (page = 1, size = 12, recent = false) => {
-  try {
-    let url = `https://clear-fashion-api.vercel.app?page=${page}&size=${size}`;
-    if (recent) {
-      url += '&recent=true';
-    }
-    const response = await fetch(url);
-    const body = await response.json();
-
-    if (body.success !== true) {
-      console.error(body);
-      return {currentProducts, currentPagination};
-    }
-
-    return body.data;
-  } catch (error) {
-    console.error(error);
-    return {currentProducts, currentPagination};
+/** Feature 2 - Filter by brands
+As a user
+I want to filter by brands name
+So that I can browse product for a specific brand */
+selectBrand.addEventListener('change', async (event) => {
+  const products = await fetchProducts(1,currentPagination.count);
+  
+  if(event.target.value != 'all'){
+    products.result = products.result.filter(product => product.brand == event.target.value);
   }
-};
-*/
-
-/**feature 4
-
-/**
- * Fetch products from api and filter by price
- * @param  {Number}  [page=1] - current page to fetch
- * @param  {Number}  [size=12] - size of the page
- * @param  {Boolean} [recent=false] - filter by recent products
- * @param  {Number}  [maxPrice=50] - filter by maximum price
- * @return {Object}
- */
-/**
-const fetchProductsAndFilterByPrice = async (page = 1, size = 12, recent = false, maxPrice = 50) => {
-  try {
-    const response = await fetch(
-      `https://clear-fashion-api.vercel.app?page=${page}&size=${size}`
-    );
-    const body = await response.json();
-
-    if (body.success !== true) {
-      console.error(body);
-      return {currentProducts, currentPagination};
-    }
-
-    const filteredProducts = body.data.result.filter(product => product.price <= maxPrice);
-
-    return {
-      result: filteredProducts,
-      meta: {
-        currentPage: body.data.currentPage,
-        pageCount: body.data.pageCount,
-        count: filteredProducts.length,
-      }
-    };
-  } catch (error) {
-    console.error(error);
-    return {currentProducts, currentPagination};
-  }
-};
-*/
-/**
- * Declaration of all Listeners
- */
-
-/**
- * Filter by reasonable price
- */
-/**
-buttonReasonablePrice.addEventListener('click', async () => {
-  const products = await fetchProductsAndFilterByPrice();
 
   setCurrentProducts(products);
   render(currentProducts, currentPagination);
 });
-*/
 
-/**feature 5
-/**
- * Sort products by price
- * @param  {Array} products
- * @param  {String} order - 'asc' for ascending or 'desc' for descending order
- */
-/**
-const sortProductsByPrice = (products, order = "asc") => {
-  return products.sort((a, b) => {
-    if (order === "asc") {
-      return a.price - b.price;
-    } else {
-      return b.price - a.price;
-    }
+/** Feature 3 - Filter by recent products
+As a user
+I want to filter by by recent products
+So that I can browse the new released products (less than 2 weeks) */
+selectReleased.addEventListener('click', async (event) => {
+  const products = await fetchProducts(1,currentPagination.count);
+  const today = new Date();
+  const twoWeeksAgo = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 14);
+  products.result = products.result.filter(product => {
+    const date = new Date(product.released);
+    return date > twoWeeksAgo;
   });
+
+  setCurrentProducts(products);
+  render(currentProducts, currentPagination);
+});
+
+/**
+Feature 4 - Filter by reasonable price
+As a user
+I want to filter by reasonable price
+So that I can buy affordable product i.e less than 50€ */
+selectPrice.addEventListener('click', async (event) => {
+  const products = await fetchProducts(1,currentPagination.count);
+  products.result = products.result.filter(product => product.price < 50);
+
+  setCurrentProducts(products);
+  render(currentProducts, currentPagination);
+});
+
+/** 
+Feature 5 - Sort by price
+As a user
+I want to sort by price
+So that I can easily identify cheapest and expensive products 
+
+AND
+
+Feature 6 - Sort by date
+As a user
+I want to sort by price
+So that I can easily identify recent and old products */
+selectSort.addEventListener('change', async (event) => {
+  const products = await fetchProducts(1,currentPagination.count);
+  if(event.target.value == 'price-asc'){
+    products.result.sort((a, b) => a.price - b.price);
+  } else if(event.target.value == 'price-desc'){
+    products.result.sort((a, b) => b.price - a.price);
+  }
+  else if(event.target.value == 'date-asc'){
+    products.result.sort((a, b) => new Date(a.released) - new Date(b.released));
+  } else if(event.target.value == 'date-desc'){
+    products.result.sort((a, b) => new Date(b.released) - new Date(a.released));
+  }
+
+  setCurrentProducts(products);
+  render(currentProducts, currentPagination);
+});
+
+/**
+Feature 8 - Number of products indicator
+As a user
+I want to indicate the total number of products
+So that I can understand how many products is available */
+/** It's alraedy done in renderIndicator 
+ * const renderIndicators = pagination => {
+  const {count} = pagination;
+  spanNbProducts.innerHTML = count;
+};
+*/
+/**
+Feature 9 - Number of recent products indicator
+As a user
+I want to indicate the total number of recent products
+So that I can understand how many new products are available */
+function getRecentProducts(products){
+  const today = new Date();
+  const twoWeeksAgo = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 14);
+  const recentProducts = products.filter(product => {
+    const date = new Date(product.released);
+    return date > twoWeeksAgo;
+  });
+  return recentProducts.length;
+}
+
+const renderNewProductsIndicator = async() => {
+  const products = await fetchProducts(1,currentPagination.count);
+  const recentProducts = getRecentProducts(products.result);
+  spanNbNewProducts.innerHTML = recentProducts;
 };
 
 /**
- * Declaration of all Listeners
- */
-
-/**
- * Sort products by price
- */
-/**
-selectSort.addEventListener("change", () => {
-  const order = selectSort.value;
-  const sortedProducts = sortProductsByPrice(currentProducts, order);
-
-  render(sortedProducts, currentPagination);
-});
-*/
-
-
-/**feature 6
-
- * Fetch products from api with optional sort by date
- * @param  {Number}  [page=1] - current page to fetch
- * @param  {Number}  [size=12] - size of the page
- * @param  {Boolean} [recent=false] - filter by recent products
- * @param  {Boolean} [sortByDate=false] - sort by date
- * @return {Object}
- */
-/**
-const fetchProducts = async (page = 1, size = 12, recent = false, sortByDate = false) => {
-  try {
-    let url = `https://clear-fashion-api.vercel.app?page=${page}&size=${size}`;
-
-    if (recent) {
-      url += '&recent=true';
-    }
-
-    if (sortByDate) {
-      url += '&sort=date';
-    }
-
-    const response = await fetch(url);
-    const body = await response.json();
-
-    if (body.success !== true) {
-      console.error(body);
-      return {currentProducts, currentPagination};
-    }
-
-    return body.data;
-  } catch (error) {
-    console.error(error);
-    return {currentProducts, currentPagination};
+Feature 10 - p50, p90 and p95 price value indicator
+As a user
+I want to indicate the p50, p90 and p95 price value
+So that I can understand the price values of the products */
+const quantile = (arr, q,attribute) => {
+  const sorted = arr.slice().sort((a, b) => a - b);
+  const pos = (sorted.length-1) * q;
+  const base = Math.floor(pos);
+  const rest = pos - base;
+  if ((sorted[base + 1][attribute] !== undefined)) {
+    return Math.round(sorted[base][attribute] + rest * (sorted[base + 1][attribute] - sorted[base][attribute]));
+  } else {
+    return Math.round(sorted[base][attribute]);
   }
 };
 
 /**
- * Sort products by date, from most recent to oldest
- */
-/**
-const sortByDate = () => {
-  currentProducts.sort((a, b) => new Date(b.date) - new Date(a.date));
+Feature 11 - Last released date indicator
+As a user
+I want to indicate the last released date
+So that I can understand if we have new products */
+const getLastReleasedDate = products => {
+  const sorted = products.slice().sort((a, b) => {
+    const dateA = new Date(a.released);
+    const dateB = new Date(b.released);
+    return dateB - dateA;
+  });
+  return sorted[0].released;
 };
 
 /**
- * Sort products by price, from lowest to highest
- */
+Feature 12 - Open product link
+As a user
+I want to open product link in a new page
+So that I can buy the product easily 
+
+We just have to add target="_blank" in the html
+*/
+
 /**
-const sortByPrice = () => {
-  currentProducts.sort((a, b) => a.price - b.price);
+Feature 13 - Save as favorite
+As a user
+I want to save a product as favorite
+So that I can retreive this product later */
+const saveFavorite = async (id) => {
+  const products = await fetchProducts(1,currentPagination.count);
+  const product = products.result.find(product => product.id == id);
+  const favorites = JSON.parse(localStorage.getItem('favorites')) || [];
+  favorites.push(product);
+  localStorage.setItem('favorites', JSON.stringify(favorites));
 };
 
-/**
- * Declaration of all Listeners
- */
+function setupFavoriteButton() {
+  const buttons = document.querySelectorAll('.star');
+  buttons.forEach(button => {
+    button.addEventListener('click', (event) => {
+      const id = event.target.dataset.id;
+      saveFavorite(id);
+    });
+  });
+}
 
 /**
- * Sort the products by price or date when clicking on the sort buttons
- */
-/**
-buttonPrice.addEventListener('click', () => {
-  sortByPrice();
-  render(currentProducts, currentPagination);
-});
-
-buttonDate.addEventListener('click', async () => {
-  const products = await fetchProducts(currentPagination.currentPage, parseInt(selectShow.value), false, true);
+Feature 14 - Filter by favorite
+As a user
+I want to filter by favorite products
+So that I can load only my favorite products */
+/*
+selectFavorite.addEventListener('click', async (event) => {
+  const favorites = JSON.parse(localStorage.getItem('favorites')) || [];
+  const products = await fetchProducts(1,currentPagination.count);
+  products.result = products.result.filter(product => favorites.find(favorite => favorite.id == product.id));
 
   setCurrentProducts(products);
-  sortByDate();
   render(currentProducts, currentPagination);
-});
+});*/
+/**
+Feature 15 - Usable and pleasant UX
+As a user
+I want to parse a usable and pleasant web page
+So that I can find valuable and useful content 
+
+We just have to add some css into the html
+*/
 
 document.addEventListener('DOMContentLoaded', async () => {
   const products = await fetchProducts();
-
   setCurrentProducts(products);
-  render(currentProducts, currentPagination);
-});
-*/
+  const allProducts = await fetchProducts(1, currentPagination.count);
 
-/**feature 9
-*/
+  const brands = await fetchBrands();
+  renderBrands(brands);
+  renderNewProductsIndicator(allProducts);
+  render(currentProducts, currentPagination);
+  const p50 = quantile(allProducts.result, 0.5, "price");
+  const p90 = quantile(allProducts.result, 0.90, "price");
+  const p95 = quantile(allProducts.result, 0.95, "price");
+  document.getElementById('p50').innerHTML = Math.round(p50*100)/100;
+  document.getElementById('p90').innerHTML = Math.round(p90*100)/100;
+  document.getElementById('p95').innerHTML = Math.round(p95*100)/100;
+  document.getElementById('lastReleasedDate').innerHTML = getLastReleasedDate(allProducts.result);
+  setupFavoriteButton();
+});
